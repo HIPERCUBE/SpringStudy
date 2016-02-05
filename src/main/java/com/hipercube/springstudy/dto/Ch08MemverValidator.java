0dc6d2886b44e53923d1b2c1ad062b98c0892078
Joowon Ryoo <joowonryoo@gmail.com>
2016-02-05 18:41:28 +0900

    Add Chapter8 DTO and Validator

---
+++ src/main/java/com/hipercube/springstudy/dto/Ch08MemverValidator.java
@@ -0,0 +1,52 @@
+package com.hipercube.springstudy.dto;
+
+import com.sun.xml.internal.ws.assembler.jaxws.ValidationTubeFactory;
+import org.springframework.validation.Errors;
+import org.springframework.validation.ValidationUtils;
+import org.springframework.validation.Validator;
+
+/**
+ * Copyright (c) 2/5/16 Joowon Ryoo
+ * <p>
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ * <p>
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ * <p>
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+public class Ch08MemverValidator implements Validator {
+    @Override
+    public boolean supports(Class<?> aClass) {
+        return Ch08Member.class.isAssignableFrom(aClass);
+    }
+
+    @Override
+    public void validate(Object target, Errors errors) {
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "memail", "required", "필수");
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "mname", "required", "필수");
+
+        Ch08Member member = (Ch08Member) target;
+        if (member.getMpassword() == null || member.getMpassword().trim().isEmpty()) {
+            errors.rejectValue("mpassword", "required", "필수");
+        } else if (member.getMpassword().length() < 5) {
+            errors.rejectValue("mpassword", "shortPassword", "5자 이상 입력");
+        }
+    }
+}
+
